41f67b93923038524b878be2c595c61799fbf419
agrezev <agrezev@gmail.com>
2013-02-26 13:01:59 +0400

    Error correction #3

--- pg/sql/01_cfg/20_main.sql
+++ pg/sql/01_cfg/20_main.sql
@@ -59,18 +59,18 @@ CREATE TABLE prop_group (
 , anno            TEXT
 );
 
-SELECT pg_c('r', 'wsd.prop_group', 'Группа владельцев свойств')
-, pg_c('c', 'wsd.prop_group.pogc', 'Код группы (Property Owner Group Code)')
-, pg_c('c', 'wsd.prop_group.pkg',  'Пакет, в котором добавлена группа')
-, pg_c('c', 'wsd.prop_group.sort', 'Порядок сортировки')
-, pg_c('c', 'wsd.prop_group.is_id_required', 'Загрузка без указания poid не используется')
-, pg_c('c', 'wsd.prop_group.name', 'Название')
-, pg_c('c', 'wsd.prop_group.anno', 'Аннотация')
+SELECT pg_c('r', 'prop_group', 'Группа владельцев свойств')
+, pg_c('c', 'prop_group.pogc', 'Код группы (Property Owner Group Code)')
+, pg_c('c', 'prop_group.pkg',  'Пакет, в котором добавлена группа')
+, pg_c('c', 'prop_group.sort', 'Порядок сортировки')
+, pg_c('c', 'prop_group.is_id_required', 'Загрузка без указания poid не используется')
+, pg_c('c', 'prop_group.name', 'Название')
+, pg_c('c', 'prop_group.anno', 'Аннотация')
 ;
 
 /* ------------------------------------------------------------------------- */
 CREATE TABLE prop_owner (
-  pogc        TEXT REFERENCES wsd.prop_group
+  pogc        TEXT REFERENCES cfg.prop_group
 , poid        INTEGER
 , pkg         TEXT NOT NULL
 , sort        INTEGER NOT NULL
@@ -79,13 +79,13 @@ CREATE TABLE prop_owner (
 , CONSTRAINT  prop_id_pkey PRIMARY KEY (pogc, poid)
 );
 
-SELECT pg_c('r', 'wsd.prop_owner', 'Владельцы свойств (Property Owner)')
-, pg_c('c', 'wsd.prop_owner.pogc', 'Код группы (Property Owner Group Code)')
-, pg_c('c', 'wsd.prop_owner.poid', 'ID владельца (Property Owner ID)')
-, pg_c('c', 'wsd.prop_owner.pkg',  'Пакет, в котором добавлена группа')
-, pg_c('c', 'wsd.prop_owner.sort', 'Порядок сортировки')
-, pg_c('c', 'wsd.prop_owner.name', 'Название')
-, pg_c('c', 'wsd.prop_owner.anno', 'Аннотация')
+SELECT pg_c('r', 'prop_owner', 'Владельцы свойств (Property Owner)')
+, pg_c('c', 'prop_owner.pogc', 'Код группы (Property Owner Group Code)')
+, pg_c('c', 'prop_owner.poid', 'ID владельца (Property Owner ID)')
+, pg_c('c', 'prop_owner.pkg',  'Пакет, в котором добавлена группа')
+, pg_c('c', 'prop_owner.sort', 'Порядок сортировки')
+, pg_c('c', 'prop_owner.name', 'Название')
+, pg_c('c', 'prop_owner.anno', 'Аннотация')
 ;
 
 /* ------------------------------------------------------------------------- */
@@ -100,11 +100,11 @@ CREATE TABLE prop_value (
 , CONSTRAINT  prop_value_pogc_poid_fkey FOREIGN KEY (pogc, poid) REFERENCES wsd.prop_owner
 );
 
-SELECT pg_c('r', 'wsd.prop_value',       'Значения свойств объектов')
-, pg_c('c', 'wsd.prop_value.pogc',       'Код группы (Property Owner Group Code)')
-, pg_c('c', 'wsd.prop_value.poid',       'ID владельца (Property Owner ID)')
-, pg_c('c', 'wsd.prop_value.code',       'Код свойства')
-, pg_c('c', 'wsd.prop_value.valid_from', 'Дата начала действия')
-, pg_c('c', 'wsd.prop_value.pkg',        'Пакет, в котором задано значение')
-, pg_c('c', 'wsd.prop_value.value',      'Значение свойства')
+SELECT pg_c('r', 'prop_value',       'Значения свойств объектов')
+, pg_c('c', 'prop_value.pogc',       'Код группы (Property Owner Group Code)')
+, pg_c('c', 'prop_value.poid',       'ID владельца (Property Owner ID)')
+, pg_c('c', 'prop_value.code',       'Код свойства')
+, pg_c('c', 'prop_value.valid_from', 'Дата начала действия')
+, pg_c('c', 'prop_value.pkg',        'Пакет, в котором задано значение')
+, pg_c('c', 'prop_value.value',      'Значение свойства')
 ;
