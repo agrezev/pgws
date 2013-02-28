483d9228df0fb8c62a456447ace4c9cc58e215de
agrezev <agrezev@gmail.com>
2013-02-28 14:00:39 +0400

    Add comments in 20_prop_wsd.sql

--- pg/sql/00_ws/00_cleanup.sql
+++ pg/sql/00_ws/00_cleanup.sql
@@ -27,3 +27,5 @@ SELECT ws.pkg_is_core_only();
 
 /* ------------------------------------------------------------------------- */
 ALTER TABLE wsd.pkg_script_protected ALTER COLUMN pkg DROP DEFAULT;
+
+
